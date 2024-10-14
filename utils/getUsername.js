330f938650ed2c4f7222dc6599342cbcdc3bb8fd
Sylphur <neosylphur@gmail.com>
2024-10-14 15:06:41 +0300

    fix: handle powershell error when correct cli args might not pass

--- utils/getUsername.js
+++ utils/getUsername.js
@@ -1,16 +1,16 @@
 export function getUsername(args) {
     const username = args.filter((arg) => {
-      return arg.includes('--username=')
+      return arg.includes('username=')
     })
     if (username.length < 1) {
-      console.log("Please enter a username in 'npm run start -- --username=your_username' format");
+      console.error("Please enter a username in 'npm run start -- --username=your_username' format \n on Windows (powershell) it might not work, run 'npm run start -- username=your_username' instead");
       process.exit(0);
     }
     if (username.length > 1) {
       console.log("Please enter only one username");
       process.exit(0);
     }
-    const user = username[0].slice(11);
+    const user = username[0].split('=')[1];
     console.log(`Welcome to the File Manager, ${user}`);
     return user;
   }
